093e97f6e8004ef03840af29c14db9d3f740005c
PiotrJander <pjander2@gmail.com>
2014-11-18 21:45:47 +0000

    Basic files added

---
+++ Grid.hs
@@ -0,0 +1,47 @@
+--import Control.Applicative
+import Data.Matrix
+
+type Cell = Int  -- in fact only 0 and 1 are permitted
+type Grid = Matrix Cell
+
+tick :: Grid -> Grid
+tick old = newState 1 1 old $ zero m n
+	where
+		m = nrows old
+		n = ncols old
+		newState i j old underConstruction
+			| j > n = newState (i+1) 1 old underConstruction
+			| i > m = underConstruction
+			| otherwise = newState i (j+1) old $ update i j old underConstruction
+
+update :: Int -> Int -> Grid -> Grid -> Grid
+update i j old underConstruction
+	| ijCell == 0 && countAlive == 3 = setElem 1 (i,j) underConstruction
+	| ijCell == 1 && (countAlive `elem` [2,3]) = setElem 1 (i,j) underConstruction
+	| otherwise = underConstruction
+	where
+		ijCell = old ! (i,j)
+		neighboursCoors = [(i-1, j-1), (i, j-1), (i+1, j-1), (i-1, j),
+						(i+1, j), (i-1, j+1), (i, j+1), (i+1, j+1)]
+		neighboursValues = map (getElem' old) neighboursCoors
+		countAlive = sum [ 1 | (Just 1) <- neighboursValues ]
+
+getElem' :: Grid -> (Int, Int) -> Maybe Cell
+getElem' g (i,j)
+	| i > 0 && j > 0 && i <= m && j <= n = Just $ getElem i j g
+	| otherwise = Nothing
+	where
+		m = nrows g
+		n = ncols g
+
+glider :: Grid
+glider = fromList 3 3 [0, 1, 0, 0, 0, 1, 1, 1, 1]
+
+test :: Grid
+test = joinBlocks (glider, zero 3 20, zero 20 3, zero 20 20)
+
+--nthStep :: Grid -> Int -> Grid
+--nthStep g n = iterate tick g !! n
+
+--prettyNthStep :: Grid -> Int -> IO ()
+--prettyNthStep = putStrLn . prettyMatrix . nthStep
---
+++ Main.hs
@@ -0,0 +1,19 @@
+import Grid
+import Graphics.Gloss
+import Graphics.Gloss.Data.ViewPort
+import Data.Matrix
+
+main = do
+	let d = InWindow "Game of Life" (300, 300) (10, 10)
+	simulate d white 2 test render tick'
+
+render :: Grid -> Picture
+render = scale 5 5 . pictures . map f . findSquares
+	where
+	    f (a,b) = polygon [(a,b), (a+1, b), (a+1, b+1), (a, b+1)]
+
+findSquares :: Grid -> [(Float, Float)]
+findSquares g = [(fromIntegral x, fromIntegral y)|x <-[1..(ncols g)], y <-[1..(nrows g)], g ! (x,y) == 1]
+
+tick' :: ViewPort -> Float -> Grid -> Grid
+tick' _ _ = tick

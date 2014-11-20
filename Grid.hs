f2b69d77dfdeeae1b91ec7e65dc7e0f8b8d67fea
PiotrJander <pjander2@gmail.com>
2014-11-20 02:02:43 +0000

    Importing patterns from RLE files added
    Both files were changed. A sample RLE file was added.
    
    Importing is done via
    
    fromFile :: FilePath -> IO Grid,
    
    where FilePath is just the name of the RLE file.

--- Grid.hs
+++ Grid.hs
@@ -1,6 +1,9 @@
 module Grid where
 
+import Data.List
 import Data.Matrix
+import Text.Regex.Posix
+import System.IO
 
 type Cell = Int  -- in fact only 0 and 1 are permitted
 type Grid = Matrix Cell
@@ -41,8 +44,97 @@ glider = fromList 3 3 [0, 1, 0, 0, 0, 1, 1, 1, 1]
 test :: Grid
 test = joinBlocks (glider, zero 3 20, zero 20 3, zero 20 20)
 
---nthStep :: Grid -> Int -> Grid
---nthStep g n = iterate tick g !! n
+nthStep :: Grid -> Int -> Grid
+nthStep g n = iterate tick g !! n
 
---prettyNthStep :: Grid -> Int -> IO ()
---prettyNthStep = putStrLn . prettyMatrix . nthStep
+-- conversion from RLE
+
+type RLE = String
+type RLELines = [String]
+type RLEData = String
+
+-- main = do
+-- 	rle <- readFile "oscillator-syntheses.rle"
+-- 	mapM print . getRowsData $ raw rle
+
+fromFile :: FilePath -> IO Grid
+fromFile filepath = do
+	rle <- readFile filepath
+	return $ fromRLE rle
+
+fromRLE :: RLE -> Grid
+fromRLE rle =
+	let
+		metadata = head $ raw rle
+		n = getN metadata
+		rows = makeRows n . getRowsData $ raw rle
+	in
+		fromLists rows
+
+raw :: RLE -> RLELines
+raw = filter (not . isComment) . lines
+
+-- getRowsData :: RLELines -> [String]
+-- getRowsData = axe '$' . removeExclamation . foldr (++) "" . tail
+-- 	where removeExclamation s = if "!" `isSuffixOf` s then init s else s
+
+isComment :: String -> Bool
+isComment str = str == "" || "#C " `isPrefixOf` str
+
+getN :: String -> Int
+getN str =
+	let
+		groups = str =~ "x[[:space:]]*=[[:space:]]*([0-9]+)" :: [[String]]
+		n = if null groups
+			then error "Invalid `x=' line."
+			else read (last . head $ groups) :: Int
+	in
+		n
+
+getRowsData :: RLELines -> RLEData
+getRowsData = foldr (++) "" . tail
+
+-- axe :: Char -> String -> [String]
+-- axe c s
+-- 	| c `elem` s = part : axe c rest'
+-- 	| otherwise  = [s]
+-- 	where
+-- 		(part, rest) = break (== c) s
+-- 		rest' = if null rest then rest else tail rest
+
+makeRows :: Int -> RLEData -> [[Cell]]
+makeRows n "" = []
+makeRows n s = addDead n row : makeRows n rest
+	where (row, rest) = makeRow s
+
+makeRow :: RLEData -> ([Cell], String)
+makeRow ""  = ([], "")
+makeRow "!" = ([], "")
+makeRow ('b' : s) = (0 : fst (makeRow s), snd (makeRow s))
+makeRow ('o' : s) = (1 : fst (makeRow s), snd (makeRow s))
+makeRow ('$' : s) = ([], s)
+makeRow s =
+	let
+		pat = "([0123456789]+)([bo$])"
+		(_, _, rest, groups) = s =~ pat :: (String, String, String, [String])
+		n = read (head groups) :: Int
+		c = head $ last groups  -- get one of bo$ as a character
+		expanded = replicate n c ++ rest
+	in
+		makeRow expanded
+ 
+addDead :: Int -> [Cell] -> [Cell]
+addDead n row = let missing = n - length row in row ++ replicate missing 0
+
+-- makeRow' :: String -> [Cell]
+-- makeRow' s = if null s then [] else
+-- 	let
+-- 		pat = "([0123456789]*)([bo])"
+-- 		(_, _, rest, groups) = s =~ pat :: (String, String, String, [String])
+-- 		nStr = if null groups then error "It's here" else head groups
+-- 		n = if null nStr then 1 else read nStr :: Int
+-- 		cell = if last groups == "b" then 0 else 1
+-- 	in
+-- 		replicate n cell ++ makeRow' rest
+
+sample = "x = 3\nbob$bbo$ooo"
--- Main.hs
+++ Main.hs
@@ -4,8 +4,9 @@ import Graphics.Gloss.Data.ViewPort
 import Data.Matrix
 
 main = do
+	glider <- fromFile "glider.rle"
 	let d = InWindow "Game of Life" (300, 300) (10, 10)
-	simulate d white 8 test render tick'
+	simulate d white 8 glider render tick'
 
 render :: Grid -> Picture
 render = scale 5 5 . pictures . map f . findSquares
@@ -13,7 +14,7 @@ render = scale 5 5 . pictures . map f . findSquares
 	    f (a,b) = polygon [(a,b), (a+1, b), (a+1, b+1), (a, b+1)]
 
 findSquares :: Grid -> [(Float, Float)]
-findSquares g = [(fromIntegral x, fromIntegral y)|x <-[1..(ncols g)], y <-[1..(nrows g)], g ! (x,y) == 1]
+findSquares g = [(fromIntegral x, fromIntegral y)|x <-[1..(nrows g)], y <-[1..(ncols g)], g ! (x,y) == 1]
 
 tick' :: ViewPort -> Float -> Grid -> Grid
 tick' _ _ = tick

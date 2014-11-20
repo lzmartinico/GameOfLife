92eb24f3ac1353bd678127cb906db7687165b388
PiotrJander <pjander2@gmail.com>
2014-11-20 20:58:21 +0000

    No longer using time as argument to chaos
    The time supplied by `simulate' is the length of one step, so it is
    constant and it is useless as argument to `chaos'.
    
    So I restored some function as they were before.
    
    But using the coordinates of a cell as input to chaos is perfect.
    
    If we do want to use the time elapsed, we'd need to use animate rather
    than simulate.

--- src/Main.hs
+++ src/Main.hs
@@ -16,10 +16,10 @@ main = do
 		fileName = printf "../Patterns/%s.rle" patternName
 	pattern <- fromFile fileName
 	let d = InWindow "Game of Life" (700, 700) (10, 10)
-	simulate d black stepsPerSec (pattern, 0) render tick'
+	simulate d black stepsPerSec pattern render tick'
 
-render :: (Grid, Time) -> Picture
-render (g, t) = translate (-200) (-200) . scale 5 5 . pictures . map f . findSquares $ g
+render :: Grid -> Picture
+render = translate (-200) (-200) . scale 5 5 . pictures . map f . findSquares
 	where
 		-- f :: Coors -> Picture
 	    f (a,b) = chaoticColor a b $ polygon [(a,b), (a+1, b), (a+1, b+1), (a, b+1)]
@@ -27,10 +27,10 @@ render (g, t) = translate (-200) (-200) . scale 5 5 . pictures . map f . findSqu
 	    stockColors = [red, green, blue, yellow, cyan, magenta, violet, rose, azure, aquamarine, orange, chartreuse]
 	    -- chaoticColor :: Float -> Float -> Picture -> Picture
 	    chaoticColor i j = color (stockColors !! chaos)
-	    	where chaos = (floor (t * i * j)) `mod` 12
+	    	where chaos = (floor (i * j)) `mod` 12
 
 findSquares :: Grid -> [Coors]
 findSquares g = [(fromIntegral x, fromIntegral y)|x <-[1..(nrows g)], y <-[1..(ncols g)], g ! (x,y) == 1]
 
-tick' :: ViewPort -> Float -> (Grid, Time) -> (Grid, Time)
-tick' _ t (g, _)  = (tick g, t)
+tick' :: ViewPort -> Float -> Grid -> Grid
+tick' _ _  = tick

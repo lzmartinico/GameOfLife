306625af7935efcbe77e7ec8c029c680bad0d130
Lorenzo Martinico <lzmartinico@gmail.com>
2014-11-21 00:32:23 +0000

    Added first step for showing an options menu

--- src/Main.hs
+++ src/Main.hs
@@ -4,24 +4,32 @@ import Graphics.Gloss.Data.ViewPort(ViewPort)
 import Data.Matrix
 import Text.Printf
 import System.Environment
+import System.Directory
 
 type Time  = Float
-type Coors = (Float, Float)
+type Colors = (Float, Float)
 
+--need to be able to alternate between pattern and fileList when recognising a command such as "Options"
 main = do
 	args <- getArgs
 	let
 		stepsPerSec = read (head args) :: Int
 		patternName = last args
-		fileName = printf "../Patterns/%s.rle" patternName
+		fileName = printf "../patterns/%s.rle" patternName
+                fileNameList = getDirectoryContents "../patterns/"
 	pattern <- fromFile fileName
-	let d = InWindow "Game of Life" (700, 700) (10, 10)
+	fileList <- fileNameList
+        let 
+            patternList = (filter f fileList)
+            f x = length x > 4
+        --return patternList
+        let d = InWindow "Game of Life" (1000, 1000) (10, 10)
 	simulate d black stepsPerSec pattern render tick'
 
 render :: Grid -> Picture
 render = translate (-200) (-200) . scale 5 5 . pictures . map f . findSquares
 	where
-		-- f :: Coors -> Picture
+		--f :: Colors -> Picture
 	    f (a,b) = chaoticColor a b $ polygon [(a,b), (a+1, b), (a+1, b+1), (a, b+1)]
 	    -- stockColors :: [Color]
 	    stockColors = [red, green, blue, yellow, cyan, magenta, violet, rose, azure, aquamarine, orange, chartreuse]
@@ -29,7 +37,7 @@ render = translate (-200) (-200) . scale 5 5 . pictures . map f . findSquares
 	    chaoticColor i j = color (stockColors !! chaos)
 	    	where chaos = (floor (i * j)) `mod` 12
 
-findSquares :: Grid -> [Coors]
+findSquares :: Grid -> [Colors]
 findSquares g = [(fromIntegral x, fromIntegral y)|x <-[1..(nrows g)], y <-[1..(ncols g)], g ! (x,y) == 1]
 
 tick' :: ViewPort -> Float -> Grid -> Grid
